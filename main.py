c3191ca6a0f48479ee746476e73926aad43618db
thanhdai0411 <ngothanhdai2001@gmail.com>
2024-09-28 10:12:58 +0700

    feat(firestore): connect, push data sensor

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+from firebase_admin import firestore
+from google.cloud.firestore_v1 import SERVER_TIMESTAMP
 
 import smhome_mqtt
 import smhome_constants
-
-# firebase constant
 
 
 cred = credentials.Certificate("sm-home-firebase-sdk.json")
@@ -14,9 +14,25 @@ firebase_admin.initialize_app(cred, {
     'databaseURL': smhome_constants.DATABASE_URL
 })
 
-
+# firebase realtime
 ref = db.reference()
-# ref.set(data)
+
+# firestore
+db_firestore = firestore.client()
+
+print(db_firestore)
+
+
+def save_sensor_firestore(sensor_id, node_id, value):
+    sensor_ref = db_firestore.collection(smhome_constants.ROOT_SM_HOME).document(node_id).collection(sensor_id)
+    sensor_ref.add({
+        "sensorId" : sensor_id,
+        "nodeId" : node_id,
+        'value': value,
+        'timestamp': SERVER_TIMESTAMP  # Tự động ghi lại thời gian hiện tại
+    })
+
+    print(f'[FireStore]:   Sensor data save ::: {sensor_id}')
 
 
 # connnect mqtt 
@@ -25,7 +41,23 @@ client.loop_start()
 
 
 def on_message(client, userdata, msg):
-    print(f"[SUB]:   Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+    topicSensor = msg.topic
+    dataSensor = msg.payload.decode()
+
+    # set realtime
+    refSensor = db.reference(topicSensor)
+    refSensor.set(dataSensor)
+
+    # spilit add firestore
+    sensorSplit = topicSensor.split("/")
+    nodeId = sensorSplit[2]
+    sensorId = sensorSplit[3]
+    save_sensor_firestore(sensorId, nodeId, dataSensor )
+
+
+    # log
+    print(f"[SUB]:   Received `{dataSensor}` from `{topicSensor}` topic")
 
 
 for topicSensor in smhome_constants.SM_HOME_ALL_SENSOR_TOPIC : 
@@ -33,9 +65,14 @@ for topicSensor in smhome_constants.SM_HOME_ALL_SENSOR_TOPIC :
 
 
 # listen firebase realtim
-def on_message(event):
+def on_message_firebase(event):
     path_all = "/"
-    if event.path != path_all:
+    path_sensor_key = "value"
+    path_sensor = event.path.split("/")[-1] # value
+
+    
+
+    if event.path != path_all and  path_sensor != path_sensor_key:
         print("---------------------------------------------------------------")
         print('[{}] :: {}'.format(event.path, event.data))
 
@@ -48,6 +85,6 @@ def on_message(event):
 
 
     
-my_stream = ref.listen(on_message)
+my_stream = ref.listen(on_message_firebase)
 
 
